4e5545b9fb39b91eb50fd31a287d24afb3e67917
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2022-11-17 22:43:12 +0200

    added static

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { hydrate, render } from "react-dom";
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
