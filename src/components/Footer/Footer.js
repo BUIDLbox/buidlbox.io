8bab6d33b095f9dff12ecc43257c528ceff6aa19
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2022-12-17 14:24:11 +0200

    fix discord link & ss

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <AnimatedLogo />
       <p>
         <a className="main-link" href="https://twitter.com/buidlbox">Twitter</a><span>{' /// '}</span>
-        <a className="main-link" href="https://twitter.com/buidlbox">Discord</a><span>{' /// '}</span>
+        <a className="main-link" href="https://discord.gg/NTRYy5V2Q9">Discord</a><span>{' /// '}</span>
         <a className="main-link" href="https://supermodular.xyz/">Supermodular</a>
       </p>
     </footer>
