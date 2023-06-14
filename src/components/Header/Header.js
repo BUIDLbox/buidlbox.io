3473dee2859bb30979fa803234b7ab0d13fcfc44
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2023-06-14 17:37:06 +0300

    add payments page link + change page description

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ function Header() {
         <Button
           text="Organize hackathons"
           small={true}
-          link={`mailto:buidlbox@supermodular.xyz?subject=buidlbox%3A%20Demo%20request&body=%F0%9F%91%8B%20Hello%20buidlbox%20team%2C%0A%0AI%20would%20like%20to%20request%20a%20demo!%20Here's%20my%20info%3A%0A%0A%3C!--%20Please%20enter%20your%20details%20below%20--%3E%0AOrganization%20name%3A%20%5BYour%20organization's%20name%5D%0AWork%20email%3A%20%5BYour%20work%20email%20address%5D%0AFull%20name%3A%20%5BYour%20name%5D%0ATime%20zone%3A%20%5BPST%2FEST%2FCET%2Fetc%5D`}
+          link={`https://app.buidlbox.io/buy-hackathons`}
         />
         <Button
           text="Launch app"
