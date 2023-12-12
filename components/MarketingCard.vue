b53a7ad9ed892273b83e43c566b7b68b85af7cb5
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2023-12-12 17:01:31 +0200

    add marketing section

---
+++ components/MarketingCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  type: "primary" | "secondary" | "tertiary";
+}>();
+
+const eyebrowClass = computed(() => {
+  const val = `text-${props.type}`;
+  return {[val]: true}
+  });
+</script>
+
+<template>
+  <div class="rounded-[32px] border border-surface py-8 px-14">
+    <p class="section-eyebrow mb-2.5" :class="eyebrowClass">
+      <slot name="eyebrow" />
+    </p>
+    <p class="font-extrabold text-2xl"><slot name="title" /></p>
+  </div>
+</template>
